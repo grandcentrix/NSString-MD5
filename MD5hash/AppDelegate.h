2e6d2751dbdf631062cab420071501864a7b9515
Michael MacCallum <maccdevteam@gmail.com>
2012-10-27 03:56:29 -0400

    Initial Commit

---
+++ MD5hash/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MD5hash
+//
+//  Created by Web on 10/27/12.
+//  Copyright (c) 2012 HappTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MD5hash/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MD5hash
+//
+//  Created by Web on 10/27/12.
+//  Copyright (c) 2012 HappTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
